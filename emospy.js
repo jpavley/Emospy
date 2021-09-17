b435d3105c070c3172141b41a2f7407fa1a07f86
John Pavley <john@pavley.com>
2021-09-16 21:43:34 -0400

    drawing emoji on the rim of the color wheel

--- emospy.js
+++ emospy.js
@@ -6,27 +6,56 @@
 import * as grafix from '../lib/grafix.js';
 import * as emoji from '../lib/emoji.js';
 
+// position on clock: (totalRadians / totalHours) * position
+// note that position 0 is 3 o'clock not 12 o'clock
+
+const colorWheel = [
+    { colorName: 'red',             clockPosition:  0 },
+    { colorName: 'tomato',          clockPosition:  1 },
+    { colorName: 'orange',          clockPosition:  2 },
+    { colorName: 'gold',            clockPosition:  3 },
+    { colorName: 'yellow',          clockPosition:  4 },
+    { colorName: 'greenyellow',     clockPosition:  5 },
+    { colorName: 'green',           clockPosition:  6 },
+    { colorName: 'steelblue',       clockPosition:  7 },
+    { colorName: 'blue',            clockPosition:  8 },
+    { colorName: 'blueviolet',      clockPosition:  9 },
+    { colorName: 'purple',          clockPosition: 10 },
+    { colorName: 'mediumvioletred', clockPosition: 11 },
+];
+
 function draw(timeStamp) {
     gw.ctx.clearRect(0, 0, gw.canvas.width, gw.canvas.height);
 
-    gw.ctx.save();
-    gw.ctx.beginPath();
-    gw.ctx.font = '25px Arial';
+    const margin = 50;
+    const centerX = gw.canvas.width/2;
+    const centerY = gw.canvas.height/2;
+    const radius = (Math.min(gw.canvas.width, gw.canvas.height) / 2) - margin;
+    const clockPositionAngle = 2.0/colorWheel.length;
+    const increment = 0.0025;
+    const fontSize = 40;
+    const offset = fontSize / 2;
 
-    const margin = 5;
-    const cols = (gw.canvas.width - (margin * 4)) / 25;
-    const rows = (gw.canvas.height - 25) / 25;
+    colorWheel.forEach( element => {
 
-    let index = 0;
-    for (let i = 0; i < rows; i += 1) {
-        for (let j = 0; j < cols; j += 1) {
-            if (index < emoji.emojiTable.length) {
-                gw.ctx.fillText(String.fromCodePoint(emoji.emojiTable[index].codePoint,0xFE0F), (j * 25) + (margin), (i * 25) + 25);
-            }
-            index += 1;
-        }
-    }
+        const angle = (clockPositionAngle * element.clockPosition);
+    
+        const startAngle = angle * Math.PI;
+        const endAngle = (angle + clockPositionAngle) * Math.PI;
+    
+        gw.ctx.beginPath();    
+        gw.ctx.arc(centerX, centerY, radius, startAngle,  endAngle);
+        gw.ctx.strokeStyle = element.colorName;
+        gw.ctx.lineWidth = 10;
+        gw.ctx.stroke();
+        gw.ctx.font = `${fontSize}px Arial`;
+        const x = (centerX - offset) + (radius * Math.cos(startAngle));
+        const y = (centerY + offset) + (radius * Math.sin(startAngle));
+        gw.ctx.fillText('🤖', x, y);
 
+        element.clockPosition += increment;
+    });
+    
     gw.requestID = requestAnimationFrame(draw);
 }
 
